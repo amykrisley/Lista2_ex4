d56aa59760a9032c0bcaeaa64bb3ffde4c2cf88b
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-05-03 18:07:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, h, a;
+
+            Console.Write("Digite a base:");
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura:");
+            h = double.Parse(Console.ReadLine());
+
+            a = b * h;
+
+            Console.WriteLine("área = {0}", a);
+
+            if (a > 100)
+                Console.WriteLine("terreno grande");
+        }
+    }
+}
